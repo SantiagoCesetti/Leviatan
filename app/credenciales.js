adacd0df7f9881eab597bb2c64e8a140ed9b7256
Agusvi18 <agustinvillar18nqn@gmail.com>
2024-11-12 15:58:19 -0300

    credeciales  y  register camios

--- app/credenciales.js
+++ app/credenciales.js
@@ -1,27 +1,29 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app";  
+import { getFirestore } from "firebase/firestore";  
+import { getAuth } from "firebase/auth"; 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDKUOx8Cux49KdARXHvhZ4-udxTFHFVO24",
-  authDomain: "leviatan-cff63.firebaseapp.com",
-  projectId: "leviatan-cff63",
-  storageBucket: "leviatan-cff63.appspot.com",
-  messagingSenderId: "372969899753",
-  appId: "1:372969899753:web:d16358459691ea6a511278",
-  measurementId: "G-BXPLNQNY2X"
-};
+const firebaseConfig = {  
+  apiKey: "AIzaSyDKUOx8Cux49KdARXHvhZ4-udxTFHFVO24",  
+  authDomain: "leviatan-cff63.firebaseapp.com",  
+  projectId: "leviatan-cff63",  
+  storageBucket: "leviatan-cff63.appspot.com",  
+  messagingSenderId: "372969899753",  
+  appId: "1:372969899753:web:d16358459691ea6a511278",  
+  measurementId: "G-BXPLNQNY2X"  
+};  
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const app = initializeApp(firebaseConfig);  
+const db = getFirestore(app);  
+const auth = getAuth(app);  
 
-export { db };
-export default app;
+export { db, auth };   
+export default app;  
 
-// Exportamos una función para inicializar Analytics solo en el cliente
-export function initializeAnalytics() {
-  if (typeof window !== 'undefined') {
-    const { getAnalytics } = require("firebase/analytics");
-    return getAnalytics(app);
-  }
-  return null;
+
+export function initializeAnalytics() {  
+  if (typeof window !== 'undefined') {  
+    const { getAnalytics } = require("firebase/analytics");  
+    return getAnalytics(app);  
+  }  
+  return null;  
 }
